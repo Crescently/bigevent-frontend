8a73cac6654a18c0ed701c864f6e4a76491da18d
liuzhen <2376355613@qq.com>
2023-12-07 18:24:07 +0800

    修改小bug

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import App from './App.vue'
 import router from './router'
 import '@/assets/main.scss'
 import pinia from '@/stores/index.js'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 app.use(pinia)
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
